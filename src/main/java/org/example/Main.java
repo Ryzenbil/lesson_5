bbc56d08f866aaac85a210eb38445fa241cf5ccf
Nikita Grebennikov <n.grebennikov599@gmail.com>
2025-09-29 13:53:40 +0500

    retry

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,27 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Dimensions dim = new Dimensions( 10, 20, 15);
-        Shipment shipment = new Shipment( dim, 5.5, "Aktau", true,
-                "QW1234", false );
-
-        Shipment copy = shipment.withNewAddress("Vienna");
-
-        System.out.println();
-        System.out.println("Original shipment: ");
-        System.out.println("Address: " + shipment.getDeliveryAddress());
-        System.out.println("Weight: " + shipment.getWeight());
-        System.out.println("Dimensions: " + shipment.getDimensions().getWidth() + "x"
-                + shipment.getDimensions().getHeight() + "x"
-                + shipment.getDimensions().getLength());
-
-
-        System.out.println("\nCopy: ");
-        System.out.println("Address: " + copy.getDeliveryAddress());
-        System.out.println("Weight: " + copy.getWeight());
-        System.out.println("Dimensions: " + copy.getDimensions().getWidth() + "x"
-                + copy.getDimensions().getHeight() + "x"
-                + copy.getDimensions().getLength());
-
     }
 }
