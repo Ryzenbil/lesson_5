9f531bdc40b50f680c5f41e3d75d3674baafff14
Aida <nakubovaaida.101@gmail.com>
2025-09-29 13:38:59 +0500

    Aida

--- src/main/java/org/example/Elevator.java
+++ src/main/java/org/example/Elevator.java
@@ -1,4 +1,42 @@
 package org.example;
 
 public class Elevator {
+    private int currentFloor = 1;
+    private int minFloor = -3;
+    private int maxfloor = 26;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxfloor = maxFloor;
+        this.currentFloor = 1;
+
+    }
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+    public void moveUp() {
+        currentFloor++;
+    }
+    public void moveDown() {
+        currentFloor--;
+    }
+
+    public void move(int floor) {
+        if (floor < minFloor || floor > maxfloor) {
+            System.out.println( "Error: floor is out of range :(" );
+            return;
+        }
+
+        while(currentFloor != floor)
+            if (floor > currentFloor) {
+                moveUp();
+            } else  {
+                moveDown();
+            }
+        System.out.println( "Current floor: " + currentFloor);
+
+    }
+
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,29 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Dimensions dim = new Dimensions( 10, 20, 15);
+        Shipment shipment = new Shipment( dim, 5.5, "Aktau", true,
+                "QW1234", false );
+
+        Shipment copy = shipment.withNewAddress("Vienna");
+
+        System.out.println();
+        System.out.println("Original shipment: ");
+        System.out.println("Address: " + shipment.getDeliveryAddress());
+        System.out.println("Weight: " + shipment.getWeight());
+        System.out.println("Dimensions: " + shipment.getDimensions().getWidth() + "x"
+                + shipment.getDimensions().getHeight() + "x"
+                + shipment.getDimensions().getLength());
+
+
+        System.out.println("\nCopy: ");
+        System.out.println("Address: " + copy.getDeliveryAddress());
+        System.out.println("Weight: " + copy.getWeight());
+        System.out.println("Dimensions: " + copy.getDimensions().getWidth() + "x"
+                + copy.getDimensions().getHeight() + "x"
+                + copy.getDimensions().getLength());
+
     }
 }
